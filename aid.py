972adfbe3a82c3471a4df6cb40215d6bd3d9b62c
mms <1605585155@qq.com>
2026-03-15 16:50:37 +0800

    上传requirements.txt文件

--- aid.py
+++ aid.py
@@ -33,25 +33,51 @@ if "messages" not in st.session_state:
 if "nick_name" not in st.session_state:
     st.session_state.nick_name = "布偶猫"
 if "nature" not in st.session_state:
-    st.session_state.nature = "可爱的猫咪"
+    st.session_state.nature = "傲娇的猫咪"
 if "current_name"   not in st.session_state:
     st.session_state.current_name = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
 sys_prompt = ("你是一个%s的%s，请按照你的性格回答问题，不能做出与你性格相反的回答。并且"
               "不能ooc")
+#加载会话数据
+def load_session_state():
+    session_list=[]
+    if os.path.exists("sessions"):
+        file_list=os.listdir("sessions")
+        for file in file_list:
+            if file.endswith(".json"):
+                session_list.append(file[:-5])
+    session_list.sort(reverse=True)
+    return session_list
+def load_sessions_state(session_name):
+    try:
+        if os.path.exists(f"sessions/{session_name}.json"):
+            # 读取会话数据
+            with open(f"sessions/{session_name}.json", "r", encoding="utf-8") as f:
+                session_data = json.load(f)
+                st.session_state.messages = session_data["messages"]
+                st.session_state.nick_name = session_data["nick_name"]
+                st.session_state.nature = session_data["nature"]
+                st.session_state.current_name = session_name
+    except Exception:
+        st.error("加载会话失败!")
+def delete_session(session_name):
+    try:
+        if os.path.exists(f"sessions/{session_name}.json"):
+            os.remove(f"sessions/{session_name}.json") # 删除文件
+            # 如果删除的是当前会话, 则需要更新消息列表
+            if session_name == st.session_state.current_name:
+                st.session_state.messages = []
+                st.session_state.current_name =datetime.strftime(datetime.now(),"%Y-%m-%d %H-%M-%S")
+    except Exception:
+        st.error("删除会话失败!")
 #连接到deepseek
 client = OpenAI(
     api_key=os.environ.get('DEEPSEEK_API_KEY'),
     base_url="https://api.deepseek.com")
-# 初始化时主动生成欢迎语
-# response = client.chat.completions.create(
-#         model="deepseek-chat",
-#         messages=[
-#             {"role": "user", "content": "你好！"}
-#
-#         ],
-#         stream=True
-#     )
+#主动生成欢迎语言（基于默认昵称和性格）
+# st.chat_message("assistant").write())
+
 #展示聊天信息
 for message in st.session_state.messages:
     st.chat_message(message["role"]).write(message["content"])
@@ -59,6 +85,7 @@ for message in st.session_state.messages:
 prompt=st.chat_input("尽情的享受吧！")
 #侧边栏
 with st.sidebar:
+    # st.subheader("会话历史")
     st.header("ai会话面板")
     if st.button("新建一个会话",width="stretch",icon="👆️"):
             #保存当前会话
@@ -68,12 +95,30 @@ with st.sidebar:
                 st.session_state.current_name=datetime.strftime(datetime.now(),"%Y-%m-%d %H-%M-%S")
                 save_session_state()
                 st.rerun()
+    st.divider()
+    st.text("会话历史：")
+    session_list=load_session_state()
+    # current_name=st.selectbox("选择会话",session_list)
+    for session in session_list:
+        col1, col2 = st.columns([4, 1])
+        with col1:
+            # 加载会话信息
+            # 三元运算符: 如果条件为真, 则返回第一个表达式的值; 否则, 返回第二个表达式的值 --> 语法: 值1 if 条件 else 值2
+            if st.button(session, width="stretch", icon="📄", key=f"load_{session}",
+                         type="primary" if session == st.session_state.current_name else "secondary"):
+                load_sessions_state(session)
+                st.rerun()
+        with col2:
+            # 删除会话信息
+            if st.button("", width="stretch", icon="❌️", key=f"delete_{session}"):
+                delete_session(session)
+                st.rerun()
 
 
     nick_name = st.text_input("昵称", placeholder="请输入昵称",value="(默认)布偶猫")
     if nick_name:
         st.session_state.nick_name = nick_name
-    nature = st.text_input("性格", placeholder="请输入性格",value="(默认)可爱的猫咪")
+    nature = st.text_input("性格", placeholder="请输入性格",value="(默认)傲娇的猫咪")
     if nature:
         st.session_state.nature = nature
 
@@ -97,3 +142,4 @@ if prompt:
         allliushi=allliushi+content
         liushi.chat_message(allliushi).write(allliushi)
     st.session_state.messages.append({"role":"assistant","content":allliushi})
+    save_session_state()
